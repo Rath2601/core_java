91259862271905ad8a33665da7029f0f06b76930
Rath2601 <arrathna26@gmail.com>
2024-08-31 08:04:43 +0530

    image of collections framework added

--- java_basics/src/com/javacollections/set/HashSetCode.java
+++ java_basics/src/com/javacollections/set/HashSetCode.java
@@ -1,7 +1,10 @@
 package com.javacollections.set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetCode {
 	public static void main(String[] args) {
-
+		Set s = new HashSet<>();
 	}
 }
